b79cdbc56a36cbbf0b2c2bbc2dc53bce1502f4fc
nuncamaria <nunezcanovas@gmail.com>
2024-03-31 17:57:18 +0200

    added accordions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var accordion = document.getElementsByClassName("accordion");
+
+for (i = 0; i < accordion.length; i++) {
+    accordion[i].addEventListener("click", function () {
+
+        this.classList.toggle("active");
+
+        var panel = this.nextElementSibling;
+        if (panel.style.display === "block") {
+            panel.style.display = "none";
+        } else {
+            panel.style.display = "block";
+        }
+    });
+}
